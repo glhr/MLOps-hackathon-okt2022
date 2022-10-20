1e054ef53c388700830c1b458a6798b96063af2a
glhr <grenblot@gmail.com>
2022-10-20 09:26:22 +0200

    save scraped data to google storage

--- 1-make-dataset/run_main.py
+++ 1-make-dataset/run_main.py
@@ -19,6 +19,13 @@ def api_reddit(req: RedditRequest):
         df = pd.read_csv("output.csv", index_col=None).fillna(value = "None")
         df.drop(df.columns[0],axis=1,inplace=True)
         df.to_gbq(req.dest, if_exists="replace") #write to the Big Query bucket specified by dest. change if you want some other behavior.
+
+        storage_client = storage.Client()
+        bucket = storage_client.bucket("dataset-csv")
+        blob = bucket.blob(f'output.csv')
+        print(f"uploading dataset CSV file")
+        blob.upload_from_filename("output.csv")
+
     except Exception as e:
         return repr(e), repr(df.columns)
     return "OK"
